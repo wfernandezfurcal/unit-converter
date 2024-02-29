d7b1aea34bcd4ead9ad2a2c4869e1c48e3c8fd28
wfernandezfurcal <ferfwill@gmail.com>
2024-02-29 08:46:25 -0400

    V3
    Css and toggle light/dark mode

---
+++ index.js
@@ -0,0 +1,27 @@
+// Dark mode toggle
+const btnTheme = document.querySelector(".btn-theme");
+
+// Determine the current theme mode
+const savedMode = localStorage.getItem("mode");
+const systemMode = window.matchMedia("(prefers-color-scheme: dark)").matches // Check user's system preferences for a mode
+  ? "dark"
+  : "light";
+const currentMode = savedMode || systemMode; // Use the saved mode or system mode
+
+// Set the theme mode based on the current mode
+document.documentElement.classList.toggle("dark", currentMode === "dark"); // Set the dark class on the root element if current mode is dark
+
+// Set icons appropriately
+btnTheme.classList.toggle("fa-sun", currentMode === "light");
+btnTheme.classList.toggle("fa-moon", currentMode === "dark");
+
+// Add an event listener to toggle the theme on click
+btnTheme.addEventListener("click", function () {
+  btnTheme.classList.toggle("triggered"); // Trigger the rotation animation
+  const isDarkMode = document.documentElement.classList.toggle("dark"); // Toggle the dark class on the root element and check if the mode is now dark
+  localStorage.setItem("mode", isDarkMode ? "dark" : "light"); // Save the current mode in local storage
+
+  // Toggle the icons
+  btnTheme.classList.toggle("fa-sun", !isDarkMode);
+  btnTheme.classList.toggle("fa-moon", isDarkMode);
+});
